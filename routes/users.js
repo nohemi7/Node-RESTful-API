c6849d9b76f0cf4b514df410d21aa808608dbedf
Nohemi Morales <nohemi@umail.ucsb.edu>
2023-01-25 14:46:30 -0800

    implemented post route for users

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,30 @@ import express from 'express';
 
 const router = express.Router();
 
+let users = [
+    {
+        firstName: "John",
+        lastName: "Doe",
+        age: 25
+    },
+    {
+        firstName: "Jane",
+        lastName: "Doe",
+        age: 24
+    }
+
+];
 // all routes here starting with /users
 router.get('/', (req, res) => { 
-    res.send('Hello');
+    console.log(users);
+    res.send(users);
+});
+
+router.post('/', (req, res) => {
+    const user = req.body;
+    users.push(user);
+
+    res.send(`User with the username ${user.firstName} added to the database!`);
 });
 
 export default router;
