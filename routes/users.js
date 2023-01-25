a5dc9b086544ccdac8593aa3339d56802dabf3bc
Nohemi Morales <nohemi@umail.ucsb.edu>
2023-01-25 15:44:09 -0800

    Seperated logic from routes into controller

--- routes/users.js
+++ routes/users.js
@@ -1,52 +1,19 @@
 import express from 'express';
-import { v4 as uuidv4 } from 'uuid';
 
-
+import { createUser, getUsers, getUser, deleteUser, updateUser } from '../controllers/users.js';
 const router = express.Router();
 
-let users = [];
 // all routes here starting with /users
-router.get('/', (req, res) => { 
-    res.send(users);
-});
+router.get('/', getUsers);
 
-router.post('/', (req, res) => {
-    const user = req.body;
+router.post('/', createUser);
 
-    users.push({ ...user, id: uuidv4() });
+router.get('/:id', getUser);
 
-    res.send(`User with the username ${user.firstName} added to the database!`);
-});
-
-router.get('/:id', (req, res) => {
-    const { id } = req.params;
-
-    const foundUser = users.find((user) => user.id == id);
-    res.send(foundUser);
-});
-
-router.delete('/:id', (req, res) => {
-    const { id } = req.params;
-    // remove form mock database array
-    users = users.filter((user) => user.id != id);
-
-    res.send(`User with the id ${id} deleted from the database!`);
-});
+router.delete('/:id', deleteUser);
 
 // Update user information
 // PATCH: partial modification VS PUT: modify whole user
-router.patch('/:id', (req, res) => {
-    const { id } = req.params;
-    const { firstName, lastName, age } = req.body;
-
-    // find user
-    const user = users.find((user) => user.id == id);
-    
-    if(firstName) user.firstName = firstName;
-    if(lastName) user.lastName = lastName;
-    if(age) user.age = age;
-    
-    res.send(`User with id ${id} has been updated!`);
-});
+router.patch('/:id', updateUser);
 
 export default router;
